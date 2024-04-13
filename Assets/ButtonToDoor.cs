8e1e49121e242eb3f9070a28e4734dadf6e1ffc3
Ro1 <rowanlesage2@gmail.com>
2024-04-12 18:53:21 -0700

    FIXED SAMMY STATION

--- Assets/ButtonToDoor.cs
+++ Assets/ButtonToDoor.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.ProBuilder.Shapes;
 using UnityEngine.UI;
 
 public class ButtonToDoor : MonoBehaviour
